8b07514365f38753a75f4cb2702154de7a7478f6
Rush_For _AC <uvaishzafri1.com>
2020-01-26 16:51:12 +0530

    first push

---
+++ app/src/main/java/com/example/phonecall/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.phonecall;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.CallLog;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URLEncoder;
+import java.util.Date;
+
+import static android.provider.LiveFolders.INTENT;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TODO ="Error" ;
+    TextView text;
+    StringBuffer stringBuffer = new StringBuffer();
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=findViewById(R.id.text);
+        text.setText(getCallDetails(this));
+        //sendEmail();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private  String getCallDetails(Context context) {
+
+        if (checkSelfPermission(Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CALL_LOG, Manifest.permission.WRITE_CALL_LOG},1);
+
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+
+        }
+        Cursor cursor = context.getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                null, null, null, CallLog.Calls.DATE + " DESC");
+        int number = cursor.getColumnIndex(CallLog.Calls.NUMBER);
+        int type = cursor.getColumnIndex(CallLog.Calls.TYPE);
+        int date = cursor.getColumnIndex(CallLog.Calls.DATE);
+        int duration = cursor.getColumnIndex(CallLog.Calls.DURATION);
+        int counter=0;
+        do{
+        //cursor.moveToNext();
+            try {
+                String phNumber = cursor.getString(number);
+                String callType = cursor.getString(type);
+                String callDate = cursor.getString(date);
+                Date callDayTime = new Date(Long.valueOf(callDate));
+                String callDuration = cursor.getString(duration);
+                String dir = null;
+                int dircode = Integer.parseInt(callType);
+                switch (dircode) {
+                    case CallLog.Calls.OUTGOING_TYPE:
+                        dir = "OUTGOING";
+                        break;
+                    case CallLog.Calls.INCOMING_TYPE:
+                        dir = "INCOMING";
+                        break;
+
+                    case CallLog.Calls.MISSED_TYPE:
+                        dir = "MISSED";
+                        break;
+                }
+                stringBuffer.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- "
+                        + dir + " \nCall Date:--- " + callDayTime
+                        + " \nCall duration in sec :--- " + callDuration);
+                stringBuffer.append("\n----------------------------------");
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            counter++;
+            if(counter==50)
+                break;
+        }while (cursor.moveToNext());
+        cursor.close();
+
+
+        return stringBuffer.toString();
+
+    }
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {"uvaishzafri1@gmail.com"};
+        String[] CC = {"bilalzafri1810@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, stringBuffer.toString());
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(MainActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
